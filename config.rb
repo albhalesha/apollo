e14754ba20eee4cf9a84a283fb8079a717cef70b
Kris Bulman <kris@bulman.ca>
2013-01-22 15:52:39 -0400

    secondary menu is now clickable

--- config.rb
+++ config.rb
@@ -34,7 +34,6 @@ require 'breakpoint'
 #add_import_path "../../../all/modules/FOO"
 #add_import_path "../../../../modules/FOO"
 
-
 ##
 ## You probably don't need to edit anything below this.
 ##
@@ -43,14 +42,13 @@ require 'breakpoint'
 # output_style = :expanded or :nested or :compact or :compressed
 output_style = (environment == :development) ? :expanded : :compressed
 
-
 # To enable relative paths to assets via compass helper functions. Since Drupal
 # themes can be installed in multiple locations, we don't need to worry about
 # the absolute path to the theme from the server root.
 relative_assets = true
 
 # To disable debugging comments that display the original location of your selectors. Uncomment:
-# line_comments = false
+line_comments = false
 
 # Pass options to sass. For development, we turn on the FireSass-compatible
 # debug_info if the firesass config variable above is true.
